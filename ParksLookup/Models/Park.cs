6c907ae05ff664ea1d5db9621205da36f9b7eebb
HR Williams <halrubinwilliams@gmail.com>
2021-07-03 17:49:00 -0700

    Add IndegenousLand property

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -6,5 +6,6 @@ namespace ParksLookup.Models
     public string Name { get; set; }
     public string Type { get; set; }
     public string Location { get; set; }
+    public string IndigenousLand { get; set; }
   }
 }
