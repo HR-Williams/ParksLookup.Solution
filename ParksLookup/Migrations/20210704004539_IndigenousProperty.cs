58c475489de2d44caa05a58c09e22b2d236b0644
HR Williams <halrubinwilliams@gmail.com>
2021-07-04 19:05:36 -0700

    Delete weather controller, make sure remote up to date with local

---
+++ ParksLookup/Migrations/20210704004539_IndigenousProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class IndigenousProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IndigenousLand",
+                table: "Parks",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IndigenousLand",
+                table: "Parks");
+        }
+    }
+}
