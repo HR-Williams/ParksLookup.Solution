b910695f175185bfab5acebf4193df81d24543a3
HR Williams <halrubinwilliams@gmail.com>
2021-07-04 19:53:46 -0700

    Seed data

--- ParksLookup/Models/ParksLookupContext.cs
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -9,6 +9,18 @@ namespace ParksLookup.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Park>()
+            .HasData(
+                new Park { ParkId = 3, Name="Alfred A. Loeb", Type = "state", Location ="Near Brookings, Oregon, United States", IndigenousLand = "Chit-dee-ni (Chetco) and Tolowa Dee-ni’"},
+                new Park { ParkId = 4, Name="Bates State Park", Type = "state", Location ="Near John Day, Oregon, United States", IndigenousLand = "Cayuse, Umatilla and Walla Walla, Confederated Tribes of Warm Springs"},
+                new Park { ParkId = 5, Name="Acadia", Type = "national", Location ="Maine", IndigenousLand = "Abenaki / Abénaquis, Penobscot, and Wabanaki Confederacy"},
+                new Park { ParkId = 6, Name="Arches", Type = "national", Location ="Utah", IndigenousLand = "Núu-agha-tʉvʉ-pʉ̱ (Ute)"},
+                new Park { ParkId = 7, Name="Badlands", Type = "national", Location ="South Dakota", IndigenousLand = "Cheyenne, Mnicoujou, Očhéthi Šakówiŋ"}
+            );
+        }
+
         public DbSet<Park> Parks { get; set; }
     }
 }
